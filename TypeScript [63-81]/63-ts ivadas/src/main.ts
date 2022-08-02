49e42d7400f9509bb2ec7fb37c6d56f9649100e3
ViliusJur <p.vilius.j@gmail.com>
2022-08-03 00:59:43 +0300

    feat(63): EsLint config

--- TypeScript [63-81]/63-ts ivadas/src/main.ts
+++ TypeScript [63-81]/63-ts ivadas/src/main.ts
@@ -1,1 +1,10 @@
-console.log('nu dw')
+const add = (a: number, b: number): number => a + b;
+
+const a: number = 7;
+const b: number = 8;
+
+console.log({ a, b });
+console.log({
+  'add(a, b)': add(a, b),
+  'add(3, 10)': add(3, 10),
+});
